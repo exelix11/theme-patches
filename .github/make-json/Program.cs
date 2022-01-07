a318dd120f16bee83454ebe673f09c9bb98ee1d3
user <13405476+exelix11@users.noreply.github.com>
2022-01-08 00:25:28 +0100

    add files

---
+++ .github/make-json/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.Json;
+
+var files = Directory.GetFiles("../../systemData").Where(x => x.EndsWith(".ips"));
+
+if (files.Any(x => Path.GetFileNameWithoutExtension(x).Any(Char.IsLower)))
+{
+    Console.Error.WriteLine("Ips file names should all be uppercase");
+    return -1;
+}
+
+Directory.CreateDirectory("deploy");
+Directory.CreateDirectory("deploy/ips");
+
+foreach (var f in files)
+{
+    File.Copy(f, "deploy/ips/" + Path.GetFileNameWithoutExtension(f));
+}
+
+var s = JsonSerializer.Serialize(new
+{
+    domain = "https://exelix11.github.io/theme-patches/ips/",
+    ids = files.Select(x => Path.GetFileNameWithoutExtension(x)).ToArray()
+});
+
+File.WriteAllText("deploy/index.json", s);
+
+return 0;
